bcfe62c2575d6dacfa87b8c17e3e93ecf0b0b63c
Ralitsa Goranova <ralitsa.goranova@nevexis.com>
2023-02-16 18:13:58 +0200

    Add project files.

---
+++ Welcome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+using Welcome.ViewModel;
+using Welcome.View;
+using Welcome.Others;
+
+namespace Welcome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User()
+            {
+                Name="Ralitsa",
+                Password="123456",
+                Role=UserRolesEnum.STUDENT
+            };
+            UserViewModel uvm=new UserViewModel(user);
+            UserView uv = new UserView(uvm);
+            uv.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Welcome/View/UserView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.ViewModel;
+namespace Welcome.View
+{
+    class UserView
+    {
+        private UserViewModel _viewModel;
+
+        public UserView(UserViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public void Display()
+        {
+            System.Console.WriteLine("Welcome");
+            System.Console.WriteLine("User:"+ _viewModel.Name);
+            System.Console.WriteLine("Role:" + _viewModel.Role);
+        }
+    }
+}
